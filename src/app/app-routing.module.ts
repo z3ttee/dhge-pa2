11f32191f3e1643f74febb1609ddf7f36e0d601d
Zitzmann <cedric.zitzmann@gmail.com>
2021-08-30 14:18:07 +0200

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,23 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { TodoInfoComponent } from './todo-info/todo-info.component';
-import { TodoListComponent } from './todo-list/todo-list.component';
 
-const routes: Routes = [
-  {
-    path: "",
-    component: TodoListComponent
-  },
-  {
-    path: "items",
-    redirectTo: "/",
-    pathMatch: "full"
-  },
-  {
-    path: "items/:id",
-    component: TodoInfoComponent
-  }
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
